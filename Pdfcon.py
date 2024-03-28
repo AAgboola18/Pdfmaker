edbda4abfc42ae2e40c884dcbe3458ba75fd3d93
Aagboola <A_Agboola18@cranford.hounslow.sch.uk>
2024-03-28 14:11:08 +0000

    the conversion app i made home from inches to metres

--- Pdfcon.py
+++ Pdfcon.py
@@ -1,3 +1,31 @@
-print("hello")
-name = input("what is your name ? ")
-print("hello", name)
+import PySimpleGUI as sg
+from convertermetres import ft_inch_to_m
+
+
+ftinch_label = sg.Text("Enter ft and inc: ")
+ftinch_input_box = sg.InputText(key="ftinch")
+
+layout = 0
+
+m_label = sg.Text("Value in metres: ")
+m_input_box = sg.InputText(key="metres")
+
+convert_btn = sg.Button("Convert", key="Convert")
+leave_btn = sg.Button("leave", key="leave")
+
+window = sg.Window(title="feet and inches to metres converter", layout=[[ftinch_label, ftinch_input_box],
+                   [m_label, m_input_box],
+                   [convert_btn, leave_btn]])
+
+while True:
+
+    event, values = window.read()
+    match event:
+        case 'convert':
+            meters = ft_inch = values["ftinch"]
+            window[("metres")].update(meters)
+            ft_inch_to_m(ft_inch)
+        case 'exit':
+            exit()
+
+    window.close()
---
+++ convertertometre.py
@@ -0,0 +1,8 @@
+def ft_inch_to_m(str_arg):
+    str_arg = str_arg.split()
+    metres = int(str_arg[0]) * 0.3048 + int(str_arg[1]) * 0.0254
+    metres = round(metres, 2)
+    return metres
+
+
+print(ft_inch_to_m("6 3"))
